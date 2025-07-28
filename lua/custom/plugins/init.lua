f7b921174351f084aa6ea278c2dac90f4030933e
Moldy <99385434+bmoldy@users.noreply.github.com>
2025-07-28 18:17:54 -0500

    Love2s

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,40 +3,52 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'lervag/vimtex',
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
-    init = function()
-      -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = 'zathura'
-      vim.g.vimtex_fold_enabled = 1
-    end,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    opts = {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = {
-        theme = { all = { ui = { bg_gutter = 'none' }, syn = { statement = '#8ba4b0', keyword = '#8ba4b0' } } },
-      },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = 'wave', -- Load "wave" theme
-      background = { -- map the value of 'background' option to a theme
-        dark = 'wave', -- try "dragon" !
-        light = 'wave',
-      },
-    },
-  },
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_fold_enabled = 1
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {
+			compile = false, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = true },
+			functionStyle = {},
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			colors = {
+				theme = { all = { ui = { bg_gutter = "none" }, syn = { statement = "#8ba4b0", keyword = "#8ba4b0" } } },
+			},
+			overrides = function(colors) -- add/modify highlights
+				return {}
+			end,
+			theme = "wave", -- Load "wave" theme
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "wave",
+			},
+		},
+	},
+	-- using lazy.nvim
+	{
+		"S1M0N38/love2d.nvim",
+		event = "VeryLazy",
+		version = "2.*",
+		opts = { path_to_love_bin = "love" },
+		keys = {
+			{ "<leader>v", ft = "lua", desc = "LÖVE" },
+			{ "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+			{ "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+		},
+	},
 }
